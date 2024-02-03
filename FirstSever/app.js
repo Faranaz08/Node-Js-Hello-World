271c7133b86668d55fb92680a0e4c6d0b7676aab
Faranaz08 <faranazannigeri3142@gmail.com>
2024-02-03 16:53:14 +0530

    Updated App

--- FirstSever/app.js
+++ FirstSever/app.js
@@ -1,35 +1,40 @@
-const http=require('http');
-const fs=require('fs')
-const server=http.createServer( (req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url=='/home'){
-        res.write('<html>');
-   res.write('<head><title>Home page</title></head>')
-   res.write('<body><h1>Welcome to home</body>')
-   res.write('</html>')
-  return  res.end()
-    }
-    if(url==='/message' && method==='POST'){
-        fs.writeFile('message.txt','DUMMY')
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-     if(url=='/about'){
-        res.write('<html>');
-   res.write('<head><title>Home page</title></head>')
-   res.write('<body><h1>Welcome to about</body>')
-   res.write('</html>')
-  return  res.end()
-    }
-   if(url=='/node'){
-   res.write('<html>');
-   res.write('<head><title>My first page</title></head>')
-   res.write('<body><h1>WElcome TO node js Project </h1></body>')
-   res.write('</html>')
-   res.end()
-  }
-});
-server.listen(4000);
+const http = require('http');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(3000);

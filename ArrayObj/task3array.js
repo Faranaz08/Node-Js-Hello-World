5217b9503c483cdb8c85cb9fd39257746420e6d9
Faranaz08 <faranazannigeri3142@gmail.com>
2024-02-03 12:10:48 +0530

    Object destructing

--- ArrayObj/task3array.js
+++ ArrayObj/task3array.js
@@ -4,3 +4,29 @@ let newarray=array.map(item =>{
     return item.replace(' ','empty string');
 });
 console.log(newarray);
+
+const obj1 = {'key1': 1}
+
+const obj2 = { ...obj1}
+
+if(obj2 === obj1){
+
+console.log('same objects')
+
+}
+
+else{
+
+console.log('different objects')
+
+}
+
+ const objj1 = {'key1': 1 , 'key2' : 2}
+
+const objj2 = { ...objj1, 'key1': 1000}
+
+
+
+console.log(objj1)
+
+console.log(objj2)
--- Destructing/objectDestruct.js
+++ Destructing/objectDestruct.js
@@ -1,16 +1,25 @@
-const person={
-    name:"Max",
-    age:28,
-    greet(){
-        console.log('Hi'+this.name)
-    }
-};
+// const person={
+//     name:"Max",
+//     age:28,
+//     greet(){
+//         console.log('Hi'+this.name)
+//     }
+// };
 
 
-const printName=({name})=>{
-    console.log(name);
+// const printName=({name})=>{
+//     console.log(name);
 
-}
-printName(person);
-const {name,age}=person;
-console.log(name,age);
+// }
+// printName(person);
+// const {name,age}=person;
+// console.log(name,age);
+
+
+const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+
+const { key1, key3} = { ...obj1}
+
+
+
+console.log(key1, key3)

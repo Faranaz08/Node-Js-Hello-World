3ad0fa6f4ea8f83ce49bb79640a4512e26df1dac
Faranaz08 <faranazannigeri3142@gmail.com>
2024-02-03 15:54:18 +0530

    Paring request

--- FirstSever/app.js
+++ FirstSever/app.js
@@ -1,21 +1,29 @@
 const http=require('http');
-
+const fs=require('fs')
 const server=http.createServer( (req,res)=>{
     const url=req.url;
+    const method=req.method;
     if(url=='/home'){
         res.write('<html>');
    res.write('<head><title>Home page</title></head>')
    res.write('<body><h1>Welcome to home</body>')
    res.write('</html>')
   return  res.end()
-    }else if(url=='/about'){
+    }
+    if(url==='/message' && method==='POST'){
+        fs.writeFile('message.txt','DUMMY')
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+     if(url=='/about'){
         res.write('<html>');
    res.write('<head><title>Home page</title></head>')
    res.write('<body><h1>Welcome to about</body>')
    res.write('</html>')
   return  res.end()
     }
-  else if(url=='/node'){
+   if(url=='/node'){
    res.write('<html>');
    res.write('<head><title>My first page</title></head>')
    res.write('<body><h1>WElcome TO node js Project </h1></body>')
